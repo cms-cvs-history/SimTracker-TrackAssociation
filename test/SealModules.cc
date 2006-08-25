2939447b48a554c1960f4b239a55d4a71bfa6515
Patrizia Azzi <sha1-23a389c18865c7a952754e6c0904f9c1813da38e@cern.ch>
2006-08-25 14:45:09 +0000

    reco/MC track association using TrackingParticle

---
+++ test/SealModules.cc
@@ -0,0 +1,10 @@
+
+#include "PluginManager/ModuleDef.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "SimTracker/TrackAssociation/test/testTrackAssociator.h"
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(testTrackAssociator)
+
+
+
