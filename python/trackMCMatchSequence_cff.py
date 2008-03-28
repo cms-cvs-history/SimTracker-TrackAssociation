97dc163236b9b15bb4c9967709e411429785a040
cmsbuild <>
2008-03-28 14:51:07 +0000

    This commit was manufactured by cvs2git to create tag 'SimTracker- TrackAssociation-CMSSW_2_1_0_pre1'.
    Sprout from master 2008-03-28 14:51:05 UTC Giuseppe Cerati <sha1-fba32708710cbf239b252728f58f96c6304a00dd@cern.ch> 'fixed association of 3 hit tracks/TPs/seeds'
    Delete:
        python/TrackAssociatorByChi2_cfi.py
        python/TrackAssociatorByHits_cfi.py
        python/TrackAssociatorByPosition_cff.py
        python/TrackAssociatorByPosition_cfi.py
        python/__init__.py
        python/allTrackMCMatch_cfi.py
        python/ctfWithMaterialTrackMCMatch_cfi.py
        python/globalMuonsMCMatch_cfi.py
        python/standAloneMuonsMCMatch_cfi.py
        python/trackMCMatchSequence_cff.py
        python/trackMCMatch_cfi.py

--- python/trackMCMatchSequence_cff.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from SimTracker.TrackAssociation.trackMCMatch_cfi import *
-from SimTracker.TrackAssociation.standAloneMuonsMCMatch_cfi import *
-from SimTracker.TrackAssociation.globalMuonsMCMatch_cfi import *
-from SimTracker.TrackAssociation.allTrackMCMatch_cfi import *
-trackMCMatchSequence = cms.Sequence(trackMCMatch*standAloneMuonsMCMatch*globalMuonsMCMatch*allTrackMCMatch)
-
